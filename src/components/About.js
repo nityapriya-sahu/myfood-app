943d083aff4df413de2b7e6ee1197fc7a4cea5fb
nityapriya-sahu <sahunityapriya@gmail.com>
2024-09-01 00:38:25 +0530

    class component done

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import UserFunction from "./UserFunction.js";
+import UserClass from "./UserClass.js";
+import UserGithub from "./UserGithub.js";
+// import UserClass from "./UserClass.js";
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      About page
+      <UserFunction name={"Nitya Priya (Function)"} />
+      <UserClass name={"Nitya Priya (Class)"} />
+      <UserGithub />
+    </div>
+  );
 };
 
 export default About;
